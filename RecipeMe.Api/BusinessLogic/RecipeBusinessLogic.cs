e8b8172e7780e0a8854f9820b3e862bf4095f280
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-08-17 11:00:12 +0300

    Implementing the project architecture.

---
+++ RecipeMe.Api/BusinessLogic/RecipeBusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using RecipeBook.ServiceLibrary.Entities;
+using RecipeBook.ServiceLibrary.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeMe.Api.BusinessLogic
+{
+    public class RecipeBusinessLogic
+    {
+        public void SaveRecipe(RecipeEntity recipeEntity)
+        {
+            var repository = new RecipeRepository();
+            repository.SaveRecipeToDatabase(recipeEntity);
+        }
+    }
+}
